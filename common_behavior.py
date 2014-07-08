4eb4facdfeb751bd9a0f3b0d4582294eb0b7e733
zyb12269205 <zhanyingbo1988@gmail.com>
2014-07-08 17:09:15 +0800

    prepare common behavior analysis
    need to re-organize file structure for re-useable purpose

--- common_behavior.py
+++ common_behavior.py
@@ -1,21 +1,6 @@
-__author__ = 'yingbozhan'
-
-import matplotlib.pyplot as plt
-import numpy as np
+import math
 import pylab as pl
-
-
-def get_current():
-    f = open('data', 'r')
-    data = {}
-    for oneday in f.readlines():
-        parts = oneday.split(',')
-        if len(parts) !=9:
-            continue
-        data[int(parts[0])] = [int(x) for x in parts[1:-1]]
-    f.close()
-    print data
-    return data
+import random
 
 def geo_mean(list_int):
     product = 1
@@ -39,8 +24,10 @@ def harmony_average(list_int):
     return len(list_int)*1.0/sum_all
 
 
-import math
-data = get_current()
+test_sample = 300
+sample = range(1, 46)
+
+data = {x:random.sample(sample,7) for x in range(0, test_sample)}
 data_av = {x:average_mean(data[x]) for x in data.keys()}
 data_ge = {x:geo_mean(data[x]) for x in data.keys()}
 data_ha = {x:harmony_average(data[x]) for x in data.keys()}
@@ -49,11 +36,3 @@ pl.plot(range(0,len(data_av.keys())), sorted(data_av.values()))
 pl.plot(range(0,len(data_ge.keys())), sorted(data_ge.values()))
 pl.plot(range(0,len(data_ha.keys())), sorted(data_ha.values()))
 pl.show()
-# x = data_geo.values()
-# hist, bins = np.histogram(x, bins=20)
-# width = 0.7 * (bins[1] - bins[0])
-# center = (bins[:-1] + bins[1:]) / 2
-# plt.bar(center, hist, align='center', width=width)
-#plt.show()
-#print sorted(data_geo.iteritems())
-
