14e31c99989cc91a17f93fe4449bfeb84f5823ec
zyb12269205 <zhanyingbo1988@gmail.com>
2014-07-08 15:19:27 +0800

    add in graph

--- data_processor.py
+++ data_processor.py
@@ -1,5 +1,7 @@
 __author__ = 'yingbozhan'
-import numpy
+
+import matplotlib.pyplot as plt
+import numpy as np
 
 def get_current():
     f = open('data', 'r')
@@ -23,5 +25,11 @@ def geo_mean(list_int):
 import math
 data = get_current()
 data_geo = {x:geo_mean(data[x]) for x in data.keys()}
+x = data_geo.values()
+hist, bins = np.histogram(x, bins=50)
+width = 0.7 * (bins[1] - bins[0])
+center = (bins[:-1] + bins[1:]) / 2
+plt.bar(center, hist, align='center', width=width)
+plt.show()
 print sorted(data_geo.iteritems())
 
